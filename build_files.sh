7035a6f9a5946410d2b53027cf3fe0a3792d76b6
myfmp <158035956+myfmp@users.noreply.github.com>
2024-09-08 18:11:23 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-
-# Install Python (if not installed)
-apt-get update
-apt-get install -y python3 python3-pip
-
-# Upgrade pip
-pip3 install --upgrade pip
-
-# Install your dependencies
+# Install other dependencies
 pip3 install -r requirements.txt
-
-# Your build commands
 python3.9 manage.py collectstatic

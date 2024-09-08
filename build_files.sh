cdfcf5ac528b07368e567070d49e96e8ee4a90c5
myfmp <158035956+myfmp@users.noreply.github.com>
2024-09-08 18:10:37 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,14 @@
-# Install other dependencies
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
-pip install -r requirements.txt
+#!/bin/bash
+
+# Install Python (if not installed)
+apt-get update
+apt-get install -y python3 python3-pip
+
+# Upgrade pip
+pip3 install --upgrade pip
+
+# Install your dependencies
+pip3 install -r requirements.txt
+
+# Your build commands
 python3.9 manage.py collectstatic
